a0f6735cb2ce5aec42c3297f4cf112f87b448893
luqmaanb <lb@xinabox.cc>
2020-05-03 15:58:26 +0200

    Fix reorder group

--- im01.ts
+++ im01.ts
@@ -1,12 +1,6 @@
-//% color=#444444 icon="\uf07b"
-//% groups=['SD CARD','LED OFF','LED ON','WINK', 'BLINK']
+//%color=#444444 icon="\uf07b"
+//%groups=['SD CARD','LED OFF','LED ON','WINK','BLINK']
 namespace IM01 {
-
-    //%block="IM01 create folder %u"
-    function createFolder(u: string): void {
-        mkdir("/sd/" + u)
-        return;
-    }
 
     //%block="IM01 size of file %u"
     //%u.defl="log.txt"
@@ -66,17 +60,17 @@ namespace IM01 {
     let enable4: boolean = true
     let enable5: boolean = true
 
-    //% blockId="turn_off_leds" block="IM01 turn off all leds"
-    //% weight=50 blockGap=8
-    //% group="LED OFF"
+    //%blockId="turn_off_leds" block="IM01 turn off all leds"
+    //%weight=50 blockGap=8
+    //%group="LED OFF"
     export function turn_off_leds() {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
 
-    //% blockId="turn_off_blink" block="IM01 turn off blinking"
-    //% weight=50 blockGap=8
-    //% group="LED OFF"
+    //%blockId="turn_off_blink" block="IM01 turn off blinking"
+    //%weight=50 blockGap=8
+    //%group="LED OFF"
     export function turn_off_blink() {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
@@ -88,43 +82,43 @@ namespace IM01 {
         enable5 = false
     }
 
-    //% blockId="turn_on_green_led" block="IM01 turn on green LED"
-    //% group="LED ON"
-    //% weight=30 blockGap=8
+    //%blockId="turn_on_green_led" block="IM01 turn on green LED"
+    //%group="LED ON"
+    //%weight=30 blockGap=8
     export function turn_on_green_led() {
         pins.analogWritePin(AnalogPin.P8, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
 
-    //% blockId="turn_on_blue_led" block="IM01 turn on blue LED"
-    //% group="LED ON"
-    //% weight=28 blockGap=8
+    //%blockId="turn_on_blue_led" block="IM01 turn on blue LED"
+    //%group="LED ON"
+    //%weight=28 blockGap=8
     export function turn_on_blue_led() {
         pins.analogWritePin(AnalogPin.P2, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P8, 0)
     }
 
-    //% blockId="turn_on_green_led_with_intensity" block="IM01 turn on green LED with intensity %intensity"
-    //% group="LED ON"
-    //% intensity.min=0 intensity.max=255 intensity.defl=128
-    //% weight=26 blockGap=8
+    //%blockId="turn_on_green_led_with_intensity" block="IM01 turn on green LED with intensity %intensity"
+    //%group="LED ON"
+    //%intensity.min=0 intensity.max=255 intensity.defl=128
+    //%weight=26 blockGap=8
     export function turn_on_green_led_with_intensity(intensity: number) {
         pins.analogWritePin(AnalogPin.P8, (1023 * intensity) / 255)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
 
-    //% blockId="turn_on_blue_led_with_intensity" block="IM01 turn on blue LED with intensity %intensity"
-    //% group="LED ON"
-    //% intensity.min=0 intensity.max=255 intensity.defl=128
-    //% weight=24 blockGap=8
+    //%blockId="turn_on_blue_led_with_intensity" block="IM01 turn on blue LED with intensity %intensity"
+    //%group="LED ON"
+    //%intensity.min=0 intensity.max=255 intensity.defl=128
+    //%weight=24 blockGap=8
     export function turn_on_blue_led_with_intensity(intensity: number) {
         pins.analogWritePin(AnalogPin.P2, (1023 * intensity) / 255)
         pins.digitalWritePin(DigitalPin.P8, 0)
     }
 
-    //% blockId="turn_on_blue_green_led" block="Turn On LED with G %g_value and B %b_value"
-    //% weight=22 blockGap=8
-    //% group="LED ON"
+    //%blockId="turn_on_blue_green_led" block="Turn On LED with G %g_value and B %b_value"
+    //%weight=22 blockGap=8
+    //%group="LED ON"
     //%g_value.min=0 g_value.max=255 g_value.defl=125
     //%b_value.min=0 b_value.max=255 b_value.defl=128
     export function turn_on_blue_green_led(g_value: number, b_value: number) {
@@ -132,9 +126,9 @@ namespace IM01 {
         pins.analogWritePin(AnalogPin.P2, (1023 * b_value) / 255)
     }
 
-    //% blockId="wink_green_led" block="IM01 wink green LED"
-    //% weight=20 blockGap=8
-    //% group="WINK"
+    //%blockId="wink_green_led" block="IM01 wink green LED"
+    //%weight=20 blockGap=8
+    //%group="WINK"
     export function wink_green_led() {
         pins.analogWritePin(AnalogPin.P8, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P2, 0)
@@ -146,9 +140,9 @@ namespace IM01 {
         basic.pause(500)
     }
 
-    //% blockId="wink_blue_led" block="IM01 wink blue LED "
-    //% weight=18 blockGap=8
-    //% group="WINK"
+    //%blockId="wink_blue_led" block="IM01 wink blue LED "
+    //%weight=18 blockGap=8
+    //%group="WINK"
     export function wink_blue_led() {
         pins.analogWritePin(AnalogPin.P2, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P8, 0)
@@ -160,10 +154,10 @@ namespace IM01 {
         basic.pause(500)
     }
 
-    //% blockId="wink_green_led_dur" block="IM01 wink green LED for %dur ms"
-    //% dur.defl=250
-    //% weight=16 blockGap=8
-    //% group="WINK"
+    //%blockId="wink_green_led_dur" block="IM01 wink green LED for %dur ms"
+    //%dur.defl=250
+    //%weight=16 blockGap=8
+    //%group="WINK"
     export function wink_green_led_dur(dur: number) {
         pins.analogWritePin(AnalogPin.P8, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P2, 0)
@@ -175,10 +169,10 @@ namespace IM01 {
         basic.pause(dur / 2)
     }
 
-    //% blockId="wink_blue_led_dur" block="IM01 wink blue LED for %dur ms"
-    //% dur.defl=250
-    //% weight=14 blockGap=8
-    //% group="WINK"
+    //%blockId="wink_blue_led_dur" block="IM01 wink blue LED for %dur ms"
+    //%dur.defl=250
+    //%weight=14 blockGap=8
+    //%group="WINK"
     export function wink_blue_led_dur(dur: number) {
         pins.analogWritePin(AnalogPin.P2, (1023 * 128) / 255)
         pins.digitalWritePin(DigitalPin.P8, 0)
@@ -190,12 +184,12 @@ namespace IM01 {
         basic.pause(dur / 2)
     }
 
-    //% blockId="wink_blue_green_led" block="IM01 wink blue %b_value green %g_value LED for %dur ms"
-    //% dur.defl=250
-    //% weight=12 blockGap=8
-    //% b_value.min=0 b_value.max=255 b_value.defl=128
-    //% g_value.min=0 g_value.max=255 g_value.defl=128
-    //% group="WINK"
+    //%blockId="wink_blue_green_led" block="IM01 wink blue %b_value green %g_value LED for %dur ms"
+    //%dur.defl=250
+    //%weight=12 blockGap=8
+    //%b_value.min=0 b_value.max=255 b_value.defl=128
+    //%g_value.min=0 g_value.max=255 g_value.defl=128
+    //%group="WINK"
     export function wink_blue_green_led(b_value: number, g_value: number, dur: number) {
         pins.analogWritePin(AnalogPin.P8, (1023 * g_value) / 255)
         pins.analogWritePin(AnalogPin.P2, (1023 * b_value) / 255)
@@ -208,9 +202,9 @@ namespace IM01 {
         basic.pause(dur / 2)
     }
 
-    //% blockId="blink_green_led" block="IM01 blink green LED"
-    //% weight=10 blockGap=8
-    //% group="BLINK"
+    //%blockId="blink_green_led" block="IM01 blink green LED"
+    //%weight=10 blockGap=8
+    //%group="BLINK"
     export function blink_green_led() {
         enable1 = true
 
@@ -228,9 +222,9 @@ namespace IM01 {
         })
     }
 
-    //% blockId="blink_blue_led" block="IM01 blink blue LED"
-    //% weight=8 blockGap=8
-    //% group="BLINK"
+    //%blockId="blink_blue_led" block="IM01 blink blue LED"
+    //%weight=8 blockGap=8
+    //%group="BLINK"
     export function blink_blue_led() {
         enable2 = true
 
@@ -248,10 +242,10 @@ namespace IM01 {
         })
     }
 
-    //% blockId="bink_green_led_period" block="IM01 blink green LED with period %dur ms"
-    //% dur.defl=500
-    //% weight=6 blockGap=8
-    //% group="BLINK"
+    //%blockId="bink_green_led_period" block="IM01 blink green LED with period %dur ms"
+    //%dur.defl=500
+    //%weight=6 blockGap=8
+    //%group="BLINK"
     export function blink_green_led_period(dur: number) {
         enable3 = true
 
@@ -269,10 +263,10 @@ namespace IM01 {
         })
     }
 
-    //% blockId="blink_blue_led_period" block="IM01 blink blue LED with period %dur ms"
-    //% dur.defl=500
-    //% weight=4 blockGap=8
-    //% group="BLINK"
+    //%blockId="blink_blue_led_period" block="IM01 blink blue LED with period %dur ms"
+    //%dur.defl=500
+    //%weight=4 blockGap=8
+    //%group="BLINK"
     export function blink_blue_led_period(dur: number) {
         enable4 = true
 
@@ -290,12 +284,12 @@ namespace IM01 {
         })
     }
 
-    //% blockId="blink_blue_green_led" block="IM01 blink blue %b_value green %g_value LED with period %dur ms"
-    //% dur.defl=500
-    //% weight=2 blockGap=8
-    //% b_value.min=0 b_value.max=255 b_value.defl=128
-    //% g_value.min=0 g_value.max=255 g_value.defl=128
-    //% group="BLINK"
+    //%blockId="blink_blue_green_led" block="IM01 blink blue %b_value green %g_value LED with period %dur ms"
+    //%dur.defl=500
+    //%weight=2 blockGap=8
+    //%b_value.min=0 b_value.max=255 b_value.defl=128
+    //%g_value.min=0 g_value.max=255 g_value.defl=128
+    //%group="BLINK"
     export function blink_blue_green_led(b_value: number, g_value: number, dur: number) {
         enable5 = true
 
@@ -312,6 +306,12 @@ namespace IM01 {
                 basic.pause(dur / 2)
             }
         })
+    }
+
+    //%block="IM01 create folder %u"
+    function createFolder(u: string): void {
+        mkdir("/sd/" + u)
+        return;
     }
 
     //%shim=im01::_mkdir
